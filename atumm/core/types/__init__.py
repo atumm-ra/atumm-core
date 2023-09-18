a3bb49daafa190923839e93d8eade4c99c470dfe
omars44 <omar@atumm.org>
2023-09-18 17:22:42 +0300

    code formatting

--- atumm/core/types/__init__.py
+++ atumm/core/types/__init__.py
@@ -1,32 +1,51 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Type, Any
-from .usecases import Command, CommandUseCase, Query, QueryUseCase
-from .presenters import AbstractPresenter
+from typing import Any, Optional, Type
 
-__all__ = ['Command', 'CommandUseCase', 'Query', 'QueryUseCase', 'AsyncContextManager', 'SyncContextManager', 'AbstractPresenter']
+from .presenters import AbstractPresenter
+from .usecases import Command, CommandUseCase, Query, QueryUseCase
+
+__all__ = [
+    "Command",
+    "CommandUseCase",
+    "Query",
+    "QueryUseCase",
+    "AsyncContextManager",
+    "SyncContextManager",
+    "AbstractPresenter",
+]
 
 
 class AsyncContextManager(ABC):
+    @abstractmethod
+    async def __aenter__(self):
+        pass
 
     @abstractmethod
-    async def __aenter__(self): pass
-    
-    @abstractmethod
-    async def __aexit__(self, exc_type: Optional[Type[BaseException]], exc_value: Optional[BaseException], traceback: Any) -> None: pass
-    
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Any,
+    ) -> None:
+        pass
+
     @classmethod
     def __subclasshook__(cls, C):
         if cls is AbstractAsyncContextManager:
-            return _collections_abc._check_methods(C, "__aenter__",
-                                                   "__aexit__")
+            return _collections_abc._check_methods(C, "__aenter__", "__aexit__")
         return NotImplemented
 
 
 class SyncContextManager(ABC):
+    @abstractmethod
+    def __enter__(self):
+        pass
 
     @abstractmethod
-    def __enter__(self): pass
-
-    @abstractmethod
-    def __exit__(self, exc_type: Optional[Type[BaseException]], exc_value: Optional[BaseException], traceback: Any) -> None: pass
-
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Any,
+    ) -> None:
+        pass
